b096da93a654e3d3c36dd1b5a72ba73d47fc7b31
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2023-12-08 16:46:05 +0200

    Modifing Keypad Configurations

--- HAL/KeyPad/KeyPad_Config.h
+++ HAL/KeyPad/KeyPad_Config.h
@@ -17,33 +17,33 @@
 
 /*Defining Ports & Pins For Rows*/
 
-#define KPD_ROW0_PORT           DIO_PORTB
+#define KPD_ROW0_PORT           DIO_PORTA
 #define KPD_ROW0_PIN            DIO_PIN0
 
-#define KPD_ROW1_PORT           DIO_PORTB
+#define KPD_ROW1_PORT           DIO_PORTA
 #define KPD_ROW1_PIN            DIO_PIN1
 
-#define KPD_ROW2_PORT           DIO_PORTB
+#define KPD_ROW2_PORT           DIO_PORTA
 #define KPD_ROW2_PIN            DIO_PIN2
 
-#define KPD_ROW3_PORT           DIO_PORTB
+#define KPD_ROW3_PORT           DIO_PORTA
 #define KPD_ROW3_PIN            DIO_PIN3
 
 /*Defining Ports & Pins For Colums*/
 
-#define KPD_COL0_PORT           DIO_PORTB
+#define KPD_COL0_PORT           DIO_PORTA
 #define KPD_COL0_PIN            DIO_PIN4
 
-#define KPD_COL1_PORT           DIO_PORTB
+#define KPD_COL1_PORT           DIO_PORTA
 #define KPD_COL1_PIN            DIO_PIN5
 
-#define KPD_COL2_PORT           DIO_PORTB
+#define KPD_COL2_PORT           DIO_PORTA
 #define KPD_COL2_PIN            DIO_PIN6
 
-#define KPD_COL3_PORT           DIO_PORTB
+#define KPD_COL3_PORT           DIO_PORTA
 #define KPD_COL3_PIN            DIO_PIN7
 
 
-#define KeyPad_Config { {'0','a','b','c'} , {5,6,7,8} , {9,10,11,12} , {13,14,15,16} }
+#define KeyPad_Config { {'7','8','9','/'} , {'4','5','6','*'} , {'1','2','3','-'} , {'C','0','=','+'} }
 
 #endif
